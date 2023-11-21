1c4dc7accde19d7b8ab6895fdd9525a8ce9ed0d0
zion <yzyrmb8@outlook.com>
2023-11-20 20:32:27 -0500

    Add ethers dependency and implement greeting functionality

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,11 +1,63 @@
+"use client";
+
 import Image from 'next/image'
+import { ethers } from 'ethers'
+import { greeterAbi, greeterAddress } from '@/interact'
+import { useState, useEffect } from 'react'
 
 export default function Home() {
+  const [greeting, setGreetingValue] = useState('')
+  const [greetingDisplay, setGreetingDisplay] = useState('')
+
+  const getGreeting = async () => {
+    const provider = new ethers.BrowserProvider(window.ethereum!)
+    const signer = await provider.getSigner()
+    const greeter = new ethers.Contract(greeterAddress, greeterAbi, signer)
+    const greeting = await greeter.greet()
+    setGreetingDisplay(greeting)
+  }
+
+  const setGreeting = async () => {
+    const provider = new ethers.BrowserProvider(window.ethereum!)
+    const signer = await provider.getSigner()
+    const greeter = new ethers.Contract(greeterAddress, greeterAbi, signer)
+    const tx = await greeter.setGreeting(greeting)
+    await tx.wait()
+  }
+
+  useEffect(() => {
+    getGreeting()
+  }, [])
+
+
+
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-6xl font-bold text-center">
         Welcome to the AA demo!
       </h1>
+      <div className="flex flex-col items-center justify-center">
+        <div>
+          <p className="text-2xl font-bold text-center">
+            The current greeting is:
+          </p>
+          <p className="text-2xl font-bold text-center">
+            {greetingDisplay}
+          </p>
+          <input
+            className="mb-4 p-2 border-2 border-black rounded-lg"
+            placeholder="Enter a greeting"
+            onChange={e => setGreetingValue(e.target.value)}
+          />
+          <button
+            className="p-2 border-2 border-black rounded-lg"
+            onClick={setGreeting}
+          >
+            Set greeting
+          </button>
+        </div>
+      </div>
     </main>
   )
 }
---
+++ frontend/interact.ts
@@ -0,0 +1,4 @@
+import greeter from '../artifacts/contracts/Greeter.sol/Greeter.json';
+
+export const greeterAddress = '0x6d3AF67c2f64d1a8ee8Fa2170Bfb125C26c08706';
+export const greeterAbi = greeter.abi;
